2a6d42251803f9668482f555cdf4777260da53df
sidharth <sidharth.delhi12@gmail.com>
2025-08-03 22:10:52 +0530

    Renamed PORT -> POSTGRE_PORT

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const db = new pg.Client({
   host: process.env.HOST,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
-  port: process.env.PORT,
+  port: process.env.POSTGRE_PORT,
 });
 db.connect();
 
